f25666433f03607d5cf48480dddb17de0349a168
Crowdbotics <team@crowdbotics.com>
2022-02-16 00:31:22 +0000

    Update models

--- candidate_scheduler_33639/wsgi.py
+++ candidate_scheduler_33639/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'candidate_scheduler_33639.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "candidate_scheduler_33639.settings")
 
 application = get_wsgi_application()
